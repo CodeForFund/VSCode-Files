746c01176bd4630679cf821ab410030666bef9a5
CodeForFund <123709656+CodeForFund@users.noreply.github.com>
2023-01-28 21:02:28 -0500

    test

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 print("Git Test")
-=======
 print("Git Test")
 print("More line added more")
-print("One more changes")
->>>>>>> New-Features
+print("One more changes again and again")
